f9513ed4d354dc3c30ff63bc97614ef608d41fa1
Ajawu David <ajawudavid@gmail.com>
2020-12-28 16:49:41 +0100

    feat: unit tests for hash triangle generation question

---
+++ chapter_2/triangle_loop.js
@@ -0,0 +1,14 @@
+/**
+ * Generates a right angled triangle of a specified height. Output should be returned not just logged
+ * @param {Number} [triangleHeight=7]
+ * @returns {String} Returns the generated triangle as a string
+ */
+const generateTriangle = (triangleHeight = 7) => {
+    let triangle = '';
+    // Enter code to generate the triangle here
+
+    return triangle;
+}
+
+
+module.exports = generateTriangle;
