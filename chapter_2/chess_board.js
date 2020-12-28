c864b3b10712fbce67fdfb98861bf6e94dacc60d
Ajawu David <ajawudavid@gmail.com>
2020-12-28 23:31:45 +0100

    feat: added tests for fizzbuzz and chessboard

---
+++ chapter_2/chess_board.js
@@ -0,0 +1,14 @@
+/**
+ * Generates a chess board of specified size. Output should be returned not just logged
+ * @param {Number} [size=8] Size of the chessboard to be generated
+ * @returns {String} Returns the generated chess board as a string
+ */
+const chessBoardGenerator = (size=8) => {
+    let chessBoard = '';
+    // Write code to genrate chess board after this line
+
+    return chessBoard;
+}
+
+chessBoardGenerator();
+module.exports = chessBoardGenerator;
---
+++ chapter_2/fizz_buzz.js
@@ -0,0 +1,17 @@
+/**
+ * Function to generate all the numbers from 1 to 100, with two exceptions.
+ * For numbers divisible by 3, generate "Fizz" instead of the number,
+ * for numbers divisible by 5 (and not 3), generate "Buzz",
+ * and for numbers divisible by 3 and 5, generate "FizzBuzz"
+ * Output should be returned not just logged
+ * 
+ * @returns {String} Returns string containing generated numbers and fizzbuzz
+ */
+const fizzBuzzGenerator = () => {
+    let fizzBuzzOutput = '';
+    // Enter code to generate fizzbuzz and numbers here
+
+    return fizzBuzzOutput;
+}
+
+module.exports = fizzBuzzGenerator;
